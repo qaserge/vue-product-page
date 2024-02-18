d52abd5c33192fdc02675e4b801f622f47416815
Sergei <qaserge@gmail.com>
2024-02-18 15:29:57 -0500

    Event Handling v-on:click

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
   data() {
     return {
+      cart: 0,
       product: "Socks",
       image: "./assets/images/socks_green.jpg",
       description: "A pair of warm, comfortable socks",
@@ -8,10 +9,20 @@ const app = Vue.createApp({
       onSale: false,
       details: ["50% cotton", "30% wool", "20% polyester"],
       variants: [
-        { id: 2234, color: "green" },
-        { id: 2235, color: "blue" },
+        { id: 2234, color: "green", image: "./assets/images/socks_green.jpg" },
+        { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg" },
       ],
-      sizes: ["S", "M", "L", "XL"],
     };
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    removeFromCart() {
+      this.cart -= 1;
+    },
+    updateImage(variantImage) {
+      this.image = variantImage;
+    },
   },
 });
