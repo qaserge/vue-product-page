3247f43951eee48a9b2ae6627b68da158750f6cf
Sergei <qaserge@gmail.com>
2024-02-18 15:13:52 -0500

    List Rendering v-for

--- main.js
+++ main.js
@@ -6,6 +6,12 @@ const app = Vue.createApp({
       description: "A pair of warm, comfortable socks",
       inventory: 55,
       onSale: false,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      variants: [
+        { id: 2234, color: "green" },
+        { id: 2235, color: "blue" },
+      ],
+      sizes: ["S", "M", "L", "XL"],
     };
   },
 });
