17a9017020a4ba0d4621da6b3c31442ec833a8e9
Sergei <qaserge@gmail.com>
2024-02-18 15:05:51 -0500

    Conditional Rendering

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            product: 'Socks',
-            image: './assets/images/socks_green.jpg', 
-            description: 'A pair of warm, comfortable socks',
-        }
-    }
-})
+  data() {
+    return {
+      product: "Socks",
+      image: "./assets/images/socks_green.jpg",
+      description: "A pair of warm, comfortable socks",
+      inventory: 55,
+      onSale: false,
+    };
+  },
+});
